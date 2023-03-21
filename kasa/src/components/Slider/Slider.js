75bc7e3e50ffb7a8139d38eae666a7ec64347d49
Mehdony <86969900+Mehdony@users.noreply.github.com>
2023-03-21 01:52:41 +0100

    Carrousel OK

--- kasa/src/components/Slider/Slider.js
+++ kasa/src/components/Slider/Slider.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState } from "react";
+import styles from "./Slider.module.css";
 
-const Slider = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Slider = (props) => {
+  const [position, setPosition] = useState(0);
+  const datas = props.datas;
+
+  const images = datas.pictures;
+  const next = () => {
+    setPosition(position + 1 > images.length - 1 ? 0 : position + 1);
+  };
+  const prev = () => {
+    setPosition(position - 1 < 0 ? images.length - 1 : position - 1);
+  };
+
+  return (
+    <section className={styles.sliderContainer}>
+      <img className={styles.sliderImage} src={images[position]} alt="images" />
+      {images.length <= 1 ? null : (
+        <>
+          <div className={styles.arrowsContainer}>
+            <div className={styles.sliderButtons} onClick={prev}>
+              <img src="/assets/left.png" alt="left arrow" />
+            </div>
+            <div className={styles.sliderButtons} onClick={next}>
+              <img src="/assets/right.png" alt="right arrow" />
+            </div>
+          </div>
+          <span className={styles.counter}>
+            {position + 1} / {images.length}
+          </span>
+        </>
+      )}
+    </section>
+  );
 };
 
 export default Slider;
--- kasa/src/pages/Logement.js
+++ kasa/src/pages/Logement.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React from "react";
+import { useParams } from "react-router-dom";
+import Slider from "../components/Slider/Slider";
+import * as data from "../MocksDatas/datas.json";
 
 
 const Logement = () => {
-    return (
-        <div>
-     
-        </div>
-    );
+  const { id } = useParams();
+  const datas = data.default.find((data) => data.id === id);
+  console.log("log de datas brut : ", datas);
+
+  return (
+    <div>
+      <Slider datas={datas} />
+    </div>
+  );
 };
 
 export default Logement;
