8bdaeb582eb034178fc06e31dcf7c165e1560629
Mehdony <86969900+Mehdony@users.noreply.github.com>
2023-03-28 01:19:01 +0200

    Ajout d'un Scrolltotop

--- kasa/src/App.js
+++ kasa/src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Navigation from "./components/Navigation/Navigation";
 import Routing from "./Routing";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop"
 
 const App = () => {
   return (
     <BrowserRouter>
+    <ScrollToTop/>
       <Navigation />
       <Routing />
       <Footer />
